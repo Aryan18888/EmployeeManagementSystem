cf173a63b668e99d3f29b64513ba6cd167be1a8b
Aryan Choudhary <aryanchy1212@gmail.com>
2025-12-23 16:35:45 +0530

    Update TaskListNumbers.jsx

--- src/components/other/TaskListNumbers.jsx
+++ src/components/other/TaskListNumbers.jsx
@@ -8,15 +8,15 @@ const TaskListNumbers = ({data}) => {
                 <h3 className='text-xl font-medium'>New Task</h3>
             </div>
             <div className='px-9 py-6 rounded-xl w-[45%] bg-blue-400'>
-                <h2 className='text-3xl font-semibold'>{data.taskCount.failed}</h2>
+                <h2 className='text-3xl font-semibold'>{data.taskCount.completed}</h2>
                 <h3 className='text-xl font-medium'>Completed Task</h3>
             </div>
             <div className='px-9 py-6 rounded-xl w-[45%] bg-green-400'>
-                <h2 className='text-3xl font-semibold'>{data.taskCount.completed}</h2>
-                <h3 className='text-xl font-medium'>Accepted Task</h3>
+                <h2 className='text-3xl font-semibold'>{data.taskCount.active}</h2>
+                <h3 className='text-xl font-medium'>Active Task</h3>
             </div>
             <div className='px-9 py-6 rounded-xl w-[45%] bg-yellow-400'>
-                <h2 className='text-3xl font-semibold'>{data.taskCount.active}</h2>
+                <h2 className='text-3xl font-semibold'>{data.taskCount.failed}</h2>
                 <h3 className='text-xl font-medium'>Failed Task</h3>
             </div>
         </div>
